8e8fba24c9b7ed63fcaf62b664342b785bc1d93e
AHMET AYDIN <ahmet.aydin@visiomex.com>
2022-06-20 19:06:59 +0300

    solving commit

---
+++ Connected Cells in a Grid/Program.cs
@@ -0,0 +1,186 @@
+﻿List<List<int>> matrix = new List<List<int>>() { new List<int>() { 1, 1, 0, 0, 0 }, new List<int>() { 0, 1, 1, 0, 0 }, 
+                                                 new List<int>() { 0, 0, 1, 0, 1 }, new List<int>() { 1, 0, 0, 0, 1 }, 
+                                                 new List<int>() { 0, 1, 0, 1, 1 } };
+
+var result = connectedCell(matrix);
+
+Console.WriteLine(result);
+
+int connectedCell(List<List<int>> matrix)
+{
+    Dictionary<int, int> regionElementsCount = new Dictionary<int, int>();
+    int lastID = 1;
+
+    int n = matrix.Count();
+    int m = matrix[0].Count();
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] != 1)
+                continue;
+
+            lastID += 1;
+            regionElementsCount[lastID] = 1;
+
+            var stack = new Stack<(int, int)>();
+
+            stack.Push((i, j));
+            var currentIndex = (i, j);
+
+            matrix[currentIndex.i][currentIndex.j] = lastID;
+
+            while (stack.Count() > 0)
+            {
+                if (isRight1(matrix, currentIndex.i, currentIndex.j, n, m))
+                {
+                    stack.Push((currentIndex.i, currentIndex.j + 1));
+                    matrix[currentIndex.i][currentIndex.j + 1] = lastID;
+                    regionElementsCount[lastID] += 1;
+                    currentIndex = (currentIndex.i, currentIndex.j + 1);
+                    continue;
+                }
+
+                if (isRightBottom1(matrix, currentIndex.i, currentIndex.j, n, m))
+                {
+                    stack.Push((currentIndex.i + 1, currentIndex.j + 1));
+                    matrix[currentIndex.i + 1][currentIndex.j + 1] = lastID;
+                    regionElementsCount[lastID] += 1;
+                    currentIndex = (currentIndex.i + 1, currentIndex.j + 1);
+                    continue;
+                }
+
+                if (isBottom1(matrix, currentIndex.i, currentIndex.j, n, m))
+                {
+                    stack.Push((currentIndex.i + 1, currentIndex.j));
+                    matrix[currentIndex.i + 1][currentIndex.j] = lastID;
+                    regionElementsCount[lastID] += 1;
+                    currentIndex = (currentIndex.i + 1, currentIndex.j);
+                    continue;
+                }
+
+                if (isBottomLeft1(matrix, currentIndex.i, currentIndex.j, n, m))
+                {
+                    stack.Push((currentIndex.i + 1, currentIndex.j - 1));
+                    matrix[currentIndex.i + 1][currentIndex.j - 1] = lastID;
+                    regionElementsCount[lastID] += 1;
+                    currentIndex = (currentIndex.i + 1, currentIndex.j - 1);
+                    continue;
+                }
+
+                if (isLeft1(matrix, currentIndex.i, currentIndex.j, n, m))
+                {
+                    stack.Push((currentIndex.i, currentIndex.j - 1));
+                    matrix[currentIndex.i][currentIndex.j - 1] = lastID;
+                    regionElementsCount[lastID] += 1;
+                    currentIndex = (currentIndex.i, currentIndex.j - 1);
+                    continue;
+                }
+
+                if (isLeftTop1(matrix, currentIndex.i, currentIndex.j, n, m))
+                {
+                    stack.Push((currentIndex.i - 1, currentIndex.j - 1));
+                    matrix[currentIndex.i - 1][currentIndex.j - 1] = lastID;
+                    regionElementsCount[lastID] += 1;
+                    currentIndex = (currentIndex.i - 1, currentIndex.j - 1);
+                    continue;
+                }
+
+                if (isTop1(matrix, currentIndex.i, currentIndex.j, n, m))
+                {
+                    stack.Push((currentIndex.i - 1, currentIndex.j));
+                    matrix[currentIndex.i - 1][currentIndex.j] = lastID;
+                    regionElementsCount[lastID] += 1;
+                    currentIndex = (currentIndex.i - 1, currentIndex.j);
+                    continue;
+                }
+
+                if (isTopRight1(matrix, currentIndex.i, currentIndex.j, n, m))
+                {
+                    stack.Push((currentIndex.i - 1, currentIndex.j + 1));
+                    matrix[currentIndex.i - 1][currentIndex.j + 1] = lastID;
+                    regionElementsCount[lastID] += 1;
+                    currentIndex = (currentIndex.i - 1, currentIndex.j + 1);
+                    continue;
+                }
+
+                currentIndex = stack.Pop();
+            }
+        }
+    }
+
+    var max = 0;
+
+    foreach (var i in regionElementsCount)
+    {
+        if (max < i.Value)
+            max = i.Value;
+    }
+
+    return max;
+}
+
+bool isRight1(List<List<int>> matrix, int i, int j, int n, int m)
+{
+    if (((j + 1) < m) && matrix[i][j + 1] == 1)
+        return true;
+
+    return false;
+}
+
+bool isRightBottom1(List<List<int>> matrix, int i, int j, int n, int m)
+{
+    if (((j + 1) < m) && ((i + 1) < n) && matrix[i + 1][j + 1] == 1)
+        return true;
+
+    return false;
+}
+
+bool isBottom1(List<List<int>> matrix, int i, int j, int n, int m)
+{
+    if (((i + 1) < n) && matrix[i + 1][j] == 1)
+        return true;
+
+    return false;
+}
+
+bool isBottomLeft1(List<List<int>> matrix, int i, int j, int n, int m)
+{
+    if (((i + 1) < n) && ((j - 1) >= 0) && matrix[i + 1][j - 1] == 1)
+        return true;
+
+    return false;
+}
+
+bool isLeft1(List<List<int>> matrix, int i, int j, int n, int m)
+{
+    if (((j - 1) >= 0) && matrix[i][j - 1] == 1)
+        return true;
+
+    return false;
+}
+
+bool isLeftTop1(List<List<int>> matrix, int i, int j, int n, int m)
+{
+    if (((i - 1) >= 0) && ((j - 1) >= 0) && matrix[i - 1][j - 1] == 1)
+        return true;
+
+    return false;
+}
+
+bool isTop1(List<List<int>> matrix, int i, int j, int n, int m)
+{
+    if (((i - 1) >= 0) && matrix[i - 1][j] == 1)
+        return true;
+
+    return false;
+}
+
+bool isTopRight1(List<List<int>> matrix, int i, int j, int n, int m)
+{
+    if (((j + 1) < m) && ((i - 1) >= 0) && matrix[i - 1][j + 1] == 1)
+        return true;
+
+    return false;
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# Connected Cells in a Grid
+
+Consider a matrix where each cell contains either a 0 or a 1. Any cell containing a 1 is called a filled cell. Two cells  are said to be connected if they are adjacent to each other horizontally, vertically, or diagonally. In the following  grid, all cells marked x are connected to the cell marked Y.
+
+xxx 
+
+XYX 
+
+XXX
+
+
+If one or more filled cells are also connected, they form a region. Note that each cell in a region is connected to  zero or more cells in the region but is not necessarily directly connected to all the other cells in the region.Given an n x in matrix, find and print the number of cells in the largest region in the matrix. Note that there may be more than one region in the matrix.For example, there are two regions in the following 3 x 3 matrix. The larger region at the top left contains 3 cells.The smaller one at the bottom right contains 1.
+
+110 
+
+100 
+
+001
+
+
+Function Description
+Complete the connectedCell function in the editor below.
+connectedCell has the following parameter(s):
+- int matrixtraml: matrix[i] represents the it'" row of the matrix
+
+
+Returns
+- int: the area of the largest region
